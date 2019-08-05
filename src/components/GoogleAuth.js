4dc61759bcd4337c09649103bec2702bf0762c05
senigami <sd@stevenandnicole.com>
2019-08-05 00:05:13 -0400

    history

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { connect } from "react-redux";
-import { signIn, signOut } from "../actions";
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-  componentDidMount() {
-    window.gapi.load("client:auth2", () => {
-      window.gapi.client
-        .init({
-          clientId:
-            "10154779473-tlokqna5qo4tlfvinos0vp9mfhhg5tsr.apps.googleusercontent.com",
-          scope: "email"
-        })
-        .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.onAuthChange(this.auth.isSignedIn.get());
-          this.auth.isSignedIn.listen(this.onAuthChange);
-        });
-    });
-  }
+	componentDidMount() {
+		window.gapi.load('client:auth2', () => {
+			window.gapi.client
+				.init({
+					clientId: '10154779473-tlokqna5qo4tlfvinos0vp9mfhhg5tsr.apps.googleusercontent.com',
+					scope: 'email',
+				})
+				.then(() => {
+					this.auth = window.gapi.auth2.getAuthInstance();
+					this.onAuthChange(this.auth.isSignedIn.get());
+					this.auth.isSignedIn.listen(this.onAuthChange);
+				});
+		});
+	}
 
-  onAuthChange = isSignedIn => {
-    if (isSignedIn) this.props.signIn();
-    else this.props.signOut();
-  };
+	onAuthChange = isSignedIn => {
+		if (isSignedIn) this.props.signIn(this.auth.currentUser.get().getId());
+		else this.props.signOut();
+	};
 
-  onSignInClick = () => {
-  	console.log(this.auth.currentUser.get().getId());
-    this.auth.signIn(this.auth.currentUser.get().getId());
-  };
-  onSignOutClick = () => {
-    this.auth.signOut();
-  };
+	onSignInClick = () => {
+		console.log(this.auth.currentUser.get().getId());
+		this.auth.signIn(this.auth.currentUser.get().getId());
+	};
+	onSignOutClick = () => {
+		this.auth.signOut();
+	};
 
-  renderAuthButton() {
-    if (this.props.isSignedIn === null) {
-      return null;
-    } else if (this.props.isSignedIn) {
-      return (
-        <button onClick={this.onSignOutClick} className="ui blue google button">
-          <i className="google icon" />
-          Sign Out
-        </button>
-      );
-    } else {
-      return (
-        <button onClick={this.onSignInClick} className="ui red google button">
-          <i className="google icon" />
-          Sign In
-        </button>
-      );
-    }
-  }
+	renderAuthButton() {
+		if (this.props.isSignedIn === null) {
+			return null;
+		} else if (this.props.isSignedIn) {
+			return (
+				<button onClick={this.onSignOutClick} className="ui blue google button">
+					<i className="google icon" />
+					Sign Out
+				</button>
+			);
+		} else {
+			return (
+				<button onClick={this.onSignInClick} className="ui red google button">
+					<i className="google icon" />
+					Sign In
+				</button>
+			);
+		}
+	}
 
-  render() {
-    return <div>{this.renderAuthButton()}</div>;
-  }
+	render() {
+		return <div>{this.renderAuthButton()}</div>;
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    isSignedIn: state.auth.isSignedIn
-  };
+	return {
+		isSignedIn: state.auth.isSignedIn,
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  { signIn, signOut }
+	mapStateToProps,
+	{ signIn, signOut }
 )(GoogleAuth);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,41 +1,71 @@
-import React from "react";
-import { fetchStreams } from "../../actions";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
-  componentDidMount() {
-    this.props.fetchStreams();
-  }
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
 
-  renderList() {
-    return this.props.streams.map(stream => {
-      return (
-        <div className="item" key={stream.id}>
-          <i className="large middle aligned icon camera"></i>
-          <div className="content">
-            <span className="header">{stream.title}</span>
-            <div className="description">{stream.description}</div>
-          </div>
-        </div>
-      );
-    });
-  }
+	renderAdmin(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">Edit</button>
+					<button className="ui button negative">Delete</button>
+				</div>
+			);
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <h2>Streams</h2>
-        <div className="ui celled list">{this.renderList()}</div>
-      </div>
-    );
-  }
+	renderList() {
+		return this.props.streams.map(stream => {
+			return (
+				<div className="item" key={stream.id}>
+					{this.renderAdmin(stream)}
+					<i className="large middle aligned icon camera" />
+					<div className="content">
+						<span className="header">{stream.title}</span>
+						<div className="description">{stream.description}</div>
+					</div>
+				</div>
+			);
+		});
+	}
+
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to="/streams/new" className="ui button primary">
+						Create Stream
+					</Link>
+				</div>
+			);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Streams</h2>
+				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+	return {
+		streams: Object.values(state.streams),
+		currentUserId: state.auth.userId,
+		isSignedIn: state.auth.isSignedIn,
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchStreams }
+	mapStateToProps,
+	{ fetchStreams }
 )(StreamList);
